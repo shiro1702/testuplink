bfc39507f6e5f4ed9e7028414dc4cf1145601f08
Ars Bar <shiro1702@gmail.com>
2017-12-21 13:49:49 +0700

    переделаннай бат файл. добавлена стартовая генерация pug файла с табуляцией и заданием класса

--- source/compo.bat
+++ source/compo.bat
@@ -7,11 +7,17 @@ rem copy *.* %1
 
 rem @echo off >"%CD%\components\%1\%1.pug"
 
-@echo div.%1>"%CD%\components\%1\%1.pug"
+@echo 1
+@echo mixin %1>"%CD%\components\%1\%1.pug"
+@echo 	div.%1>>"%CD%\components\%1\%1.pug"
+
 @echo .%1 { }>"%CD%\components\%1\%1.scss"
+
 @echo import './%1.scss';>"%CD%\components\%1\%1.js"
 
-Echo.>>"существующий_файл.расширение"
-Echo import '../components/%1/%1.scss';>>"%CD%\index\compoImp.js"
+Echo.>>"%CD%\index\compoImp.js"
+Echo import '../components/%1/%1.scss';>>"%CD%\index\compoIn.js"
+Echo.>>"%CD%\index\compoIn.pug"
+Echo include ../components/%1/%1.pug>>"%CD%\index\compoIn.pug"
 
 echo end "create component"
