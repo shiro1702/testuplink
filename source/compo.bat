e74656144afe31f8cb9d4ac1afc7fee914ad096a
Ars Bar <shiro1702@gmail.com>
2017-12-21 17:40:41 +0700

    добавлено автоматическое создане block внутри блока

--- source/compo.bat
+++ source/compo.bat
@@ -9,15 +9,16 @@ rem @echo off >"%CD%\components\%1\%1.pug"
 
 @echo 1
 @echo mixin %1>"%CD%\components\%1\%1.pug"
-@echo 	div.%1>>"%CD%\components\%1\%1.pug"
+@echo 	div.%1&attributes(attributes)>>"%CD%\components\%1\%1.pug"
+@echo 		block>>"%CD%\components\%1\%1.pug"
 
 @echo .%1 { }>"%CD%\components\%1\%1.scss"
 
 @echo import './%1.scss';>"%CD%\components\%1\%1.js"
 
-Echo.>>"%CD%\index\compoImp.js"
+rem Echo.>>"%CD%\index\compoIn.js"
 Echo import '../components/%1/%1.scss';>>"%CD%\index\compoIn.js"
-Echo.>>"%CD%\index\compoIn.pug"
+rem Echo.>>"%CD%\index\compoIn.pug"
 Echo include ../components/%1/%1.pug>>"%CD%\index\compoIn.pug"
 
 echo end "create component"
